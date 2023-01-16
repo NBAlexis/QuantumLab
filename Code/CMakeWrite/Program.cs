57fa13efec79d59c810ab56f217ea7a19af3f07f
Ji-Chong Yang <nbalexis@gmail.com>
2023-01-17 02:43:09 +0800

    fix catch problem

--- Code/CMakeWrite/Program.cs
+++ Code/CMakeWrite/Program.cs
@@ -186,7 +186,7 @@ namespace CMakeWrite
                 if (m_sName.Equals("tests"))
                 {
                     sRet += string.Format("\ntarget_include_directories({0} PRIVATE {1})\n\n", m_sName, "${CMAKE_SOURCE_DIR}/../QuEST340/QuEST/include");
-                    sRet += string.Format("\ntarget_include_directories({0} PRIVATE {1})\n\n", m_sName, "${CMAKE_SOURCE_DIR}/../QuEST340/tests/Catch");
+                    sRet += string.Format("\ntarget_include_directories({0} PRIVATE {1})\n\n", m_sName, "${CMAKE_SOURCE_DIR}/../QuEST340/tests/catch");
                 }
 
                 sRet += string.Format("target_compile_features({0} PUBLIC cxx_std_14)\n", sAppName);
